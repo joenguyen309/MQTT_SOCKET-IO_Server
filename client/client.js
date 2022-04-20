b671fc0be08bed2232d2199ae7ebbb05a38535da
joenguyen309 <74529480+joenguyen309@users.noreply.github.com>
2022-04-20 22:33:14 +0700

    fix chart bug

--- client/client.js
+++ client/client.js
@@ -1,18 +1,109 @@
-var temp = 0.0
-var hub = 0.0
+$(function () {
+    const socket = io()
+    var label = [];
 
-$(document).ready(function () {
-    var socket = io();
 
-    socket.on('send_server', function (data) {
+    var datasetHumi = [];
+    var datasetTemp = [];
 
+    const data = {
+        labels: label,
+        datasets: [
+          {
+            label: "Humi",
+            backgroundColor: "blue",
+            borderColor: "blue",
+            data: datasetHumi,
+          },
+          {
+            label: "Temp",
+            backgroundColor: "red",
+            borderColor: "red",
+            data: datasetTemp,
+          },
+        ],
+    };
+    const data_temp = {
+    labels: label,
+    datasets: [
+        {
+        label: "Temp",
+        backgroundColor: "red",
+        borderColor: "red",
+        data: datasetTemp,
+        }
+    ],
+    };
+
+    const data_hum = {
+        labels: label,
+        datasets: [
+            {
+            label: "Humi",
+            backgroundColor: "blue",
+            borderColor: "blue",
+            data: datasetHumi,
+            }
+        ],
+        };
+
+
+    // <block:config:0>
+
+    const config = {
+        type: "line",
+        data,
+        options: {},
+        };
+        
+    const config_temp = {
+        type: "line",
+        data_temp,
+        options: {},
+    };
+
+    const config_hum = {
+        type: "line",
+        data_hum,
+        options: {},
+    };
+
+    var myChart = new Chart($("#myChart"), config);
+
+    // var myChart_temp = new Chart($("#myChart_temp"), config_temp);
+    var myChart_hum =  new Chart($("#myChart_hum"),  config_hum);
+
+    var time = new Date
+
+    var count = 0
+
+
+    function FormatDate(time) {
+
+    var date = new Date(time);
+
+    return [date.getDate() + '/' + date.getMonth() + '/' + date.getFullYear(), date.getHours() + ':' + date.getMinutes() + ':' + date.getSeconds()]
+
+    }
+
+    label.unshift(FormatDate(time))
+
+    // socket.on("firstConnect", (data) => {
+    //   data.forEach(element => { //10 9 8 7 6 =>  7 8 9 10 11
+    //     label.unshift(FormatDate(element.time));
+    //     datasetHumi.unshift(element.humi);
+    //     datasetTemp.unshift(element.temp);
+    //   });
+    //   console.log(label)
+    //   myChart.update()
+    // })
+
+    socket.on("send_server", (data) => {
         temp = parseFloat(data.split("&")[0]);
         hub = parseFloat(data.split("&")[1]);
-        
-        console.log("Temp: %s",temp)
-        console.log("------------------")
-        console.log("Humidity: %s",hub)
-        console.log("------------------")
+        // console.log(time)
+        Update(time, hub, temp,count)
+        count = count+1
     })
 
     $('#button').ready(function () {
@@ -37,4 +128,30 @@ $(document).ready(function () {
             })
         })
     })
-});
+
+    
+    function Update(newLabel, newHumi, newTemp,newCount) {
+        if (newCount <10) {
+            label.push(FormatDate(newLabel))
+            datasetHumi.push(newHumi)
+            datasetTemp.push(newTemp)
+            // myChart_temp.update()
+            myChart_hum.update()
+            myChart.update()
+        }
+        else {
+            label.shift()
+            label.push(FormatDate(newLabel))
+
+
+            datasetHumi.shift()
+            datasetHumi.push(newHumi)
+            datasetTemp.shift()
+            datasetTemp.push(newTemp)
+
+            // myChart_temp.update()
+            myChart_hum.update()
+            myChart.update()
+        }
+    }
+})
